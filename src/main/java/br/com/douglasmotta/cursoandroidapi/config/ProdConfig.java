99ac8b84fcd95e8eae7beab1efded5c454c99062
Douglas Motta <tibeca@gmail.com>
2020-12-20 02:11:09 +0000

    Altera Procfile

---
+++ src/main/java/br/com/douglasmotta/cursoandroidapi/config/ProdConfig.java
@@ -0,0 +1,39 @@
+package br.com.douglasmotta.cursoandroidapi.config;
+
+import br.com.douglasmotta.cursoandroidapi.services.DBService;
+import br.com.douglasmotta.cursoandroidapi.services.EmailService;
+import br.com.douglasmotta.cursoandroidapi.services.SmtpEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
